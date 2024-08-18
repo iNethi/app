890c94ae8b240e6d5519cf785c75adbb0c80eb70
Reabetsoe Mpapa <reabetsoempapa44@gmail.com>
2024-08-18 22:59:29 +0200

    Add Hotspot method

--- inethi/pages/HomePage.js
+++ inethi/pages/HomePage.js
@@ -46,10 +46,49 @@ const HomePage = ({logout}) => {
         url: '', // Add an empty URL field to maintain structure
       },
     ],
-    Navigator: [
-      { name: "FindHotspot", action: () => handleFindHotspotClick() },
-    ],
+    Navigator: [{name: 'FindHotspot', action: () => handleFindHotspotClick()}],
   });
+  const handleFindHotspotClick = () => {
+    const eventName = 'find_hotspot_button_clicked';
+
+    // Log event to Firebase Analytics
+    analytics()
+      .logEvent(eventName, {
+        button: 'FindHotspot',
+      })
+      .then(() => {
+        console.log(`Firebase Analytics event logged: ${eventName}`);
+      })
+      .catch(error => {
+        console.error(`Error logging event to Firebase Analytics: ${error}`);
+      });
+
+    // Log event to Amplitude
+    amplitude.track(eventName, {
+      button: 'FindHotspot',
+    });
+
+    const navigateEventName = 'navigate_to_map';
+
+    // Log event to Firebase Analytics
+    analytics()
+      .logEvent(navigateEventName, {
+        feature: 'Map',
+      })
+      .then(() => {
+        console.log(`Firebase Analytics event logged: ${navigateEventName}`);
+      })
+      .catch(error => {
+        console.error(`Error logging event to Firebase Analytics: ${error}`);
+      });
+
+    // Log event to Amplitude
+    amplitude.track(navigateEventName, {
+      feature: 'Map',
+    });
+
+    navigate('/map');
+  };
 
   const timeout = ms =>
     new Promise((_, reject) =>
